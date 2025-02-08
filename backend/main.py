c46cf92c3aa60c80bf5b309da633b436560c60ce
Ronny Ascencio <ronny.ascencio@icloud.com>
2025-02-08 17:54:46 -0500

    task handle  database all task and task filter by id endpoints

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from typing import Annotated
 
 import models
 from database import SessionLocal, engine
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException
 from models import Tasks
 from sqlalchemy.orm import Session
 
@@ -22,7 +22,15 @@ def get_db():
     finally:
         db.close_all()
 
+db_dependecy = Annotated[Session, Depends(get_db)]
 
 @app.get("/")
-async def read_all(db: Annotated[Session, Depends(get_db)]):
+async def read_all(db: db_dependecy):
     return db.query(Tasks).all()
+
+@app.get("/task/{task_id}")
+async def get_task_id(db: db_dependecy, task_id: int):
+    task_model = db.query(Tasks).filter(Tasks.id == task_id).first()
+    if task_model is not None:
+        return task_model
+    raise HTTPException(status_code=404, detail="Task not found")
